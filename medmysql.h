0bcc0bd7dcc450b20f2254a7bf26ceadd5711629
Richard Fuchs <rfuchs@sipwise.com>
2017-11-08 09:52:18 -0500

    TT#24261 eliminate bulk SQL statements
    Change-Id: I9f3d48a4d5407f1a46584602a46e05db3c33608a

--- medmysql.h
+++ medmysql.h
@@ -6,21 +6,7 @@
 #include "mediator.h"
 #include "cdr.h"
 
-#define PACKET_SIZE (1024*1024)
-
 #define STAT_PERIOD_SIZE 30
-
-struct medmysql_str {
-	char str[PACKET_SIZE];
-	unsigned int len;
-};
-
-struct medmysql_batches {
-	struct medmysql_str cdrs;
-	struct medmysql_str acc_backup;
-	struct medmysql_str acc_trash;
-	struct medmysql_str to_delete;
-};
 
 struct medmysql_call_stat_info_t {
 	char period[STAT_PERIOD_SIZE];
@@ -32,14 +18,14 @@ int medmysql_init();
 void medmysql_cleanup();
 med_callid_t *medmysql_fetch_callids(u_int64_t *count);
 int medmysql_fetch_records(med_callid_t *callid, med_entry_t **entries, u_int64_t *count);
-int medmysql_trash_entries(const char *callid, struct medmysql_batches *);
-int medmysql_backup_entries(const char *callid, struct medmysql_batches *);
-int medmysql_delete_entries(const char *callid, struct medmysql_batches *);
-int medmysql_insert_cdrs(cdr_entry_t *records, u_int64_t count, struct medmysql_batches *);
+int medmysql_trash_entries(const char *callid);
+int medmysql_backup_entries(const char *callid);
+int medmysql_delete_entries(const char *callid);
+int medmysql_insert_cdrs(cdr_entry_t *records, u_int64_t count);
 int medmysql_load_maps(GHashTable *ip_table, GHashTable *host_table, GHashTable *id_table);
 int medmysql_load_uuids(GHashTable *uuid_table);
-int medmysql_batch_start(struct medmysql_batches *);
-int medmysql_batch_end(struct medmysql_batches *);
-int medmysql_update_call_stat_info(const char *call_code, const double start_time, struct medmysql_batches *batches);
+int medmysql_batch_start();
+int medmysql_batch_end();
+int medmysql_update_call_stat_info(const char *call_code, const double start_time);
 
 #endif /* _MED_MYSQL_H */
