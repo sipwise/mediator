530fe333b91171318735ce5b907973c2386735c9
Richard Fuchs <rfuchs@sipwise.com>
2020-03-13 10:50:24 -0400

    TT#76674 add support for concurrent calls tags
    Change-Id: I26a1eb8f3e05969ca823b9c931a17da8b1401993

--- medmysql.c
+++ medmysql.c
@@ -1103,6 +1103,26 @@ int medmysql_insert_cdrs(cdr_entry_t *entries, uint64_t count, struct medmysql_b
                 return -1;
         }
 
+        if (medmysql_tag_cdr(batch, medmysql_tag_provider_customer, medmysql_tag_direction_source,
+                    "concurrent_calls_quota", e->source_concurrent_calls_quota, e))
+            return -1;
+        if (medmysql_tag_cdr(batch, medmysql_tag_provider_customer, medmysql_tag_direction_source,
+                    "concurrent_calls_count", e->source_concurrent_calls_count, e))
+            return -1;
+        if (medmysql_tag_cdr(batch, medmysql_tag_provider_customer, medmysql_tag_direction_source,
+                    "concurrent_calls_count_customer", e->source_concurrent_calls_count_customer, e))
+            return -1;
+
+        if (medmysql_tag_cdr(batch, medmysql_tag_provider_customer, medmysql_tag_direction_destination,
+                    "concurrent_calls_quota", e->destination_concurrent_calls_quota, e))
+            return -1;
+        if (medmysql_tag_cdr(batch, medmysql_tag_provider_customer, medmysql_tag_direction_destination,
+                    "concurrent_calls_count", e->destination_concurrent_calls_count, e))
+            return -1;
+        if (medmysql_tag_cdr(batch, medmysql_tag_provider_customer, medmysql_tag_direction_destination,
+                    "concurrent_calls_count_customer", e->destination_concurrent_calls_count_customer, e))
+            return -1;
+
         if (e->mos.filled && batch->mos.def) {
             if (medmysql_mos_record(&batch->cdr_mos, batch->num_cdrs, e->mos.avg_score,
                         e->mos.avg_packetloss, e->mos.avg_jitter, e->mos.avg_rtt,
