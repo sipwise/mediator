235a1aa8333e5f3b915006200f27ca7a5df4fe25
Richard Fuchs <rfuchs@sipwise.com>
2021-08-02 08:34:29 -0400

    TT#98401 refactor list of CDR field names
    Use a macro in combination with an included file to define the list of
    all string fields used in the CDR struct.
    
    Change-Id: Ic0b93c005b792eadf00544768c74382fa3307577

--- cdr.h
+++ cdr.h
@@ -25,47 +25,18 @@ typedef struct {
     int avg_rtt;
 } mos_data_t;
 
-typedef struct {
-    char call_id[128];
+#define F(f, l) char f[l];
+#define FA(f, a, l) char f[a][l];
 
-    char source_user_id[37];
-    char source_ext_subscriber_id[256];
-    char source_ext_contract_id[256];
-    char source_provider_id[256];
+typedef struct {
+#include "cdr_field_names.inc"
+
     uint64_t source_account_id;
-    char source_user[256];
-    char source_domain[256];
-    char source_cli[65];
-    char source_ip[65];
     uint8_t source_clir;
     uint64_t source_lcr_id;
-    char source_gpp[10][255];
-    char source_lnp_prefix[256];
-    char source_user_out[256];
-    char source_lnp_type[256];
 
-    char destination_user_id[37];
-    char destination_provider_id[256];
-    char destination_ext_subscriber_id[256];
-    char destination_ext_contract_id[256];
     uint64_t destination_account_id;
-    char destination_user[256];
-    char destination_domain[256];
-    char destination_user_in[256];
-    char destination_domain_in[256];
-    char destination_dialed[256];
     uint64_t destination_lcr_id;
-    char destination_gpp[10][255];
-    char destination_lnp_prefix[256];
-    char destination_user_out[256];
-    char destination_lnp_type[256];
-
-    char call_type[8];
-    char call_status[16];
-    char call_code[4];
-
-    char peer_auth_user[256];
-    char peer_auth_realm[256];
 
     double init_time;
     double start_time;
@@ -78,28 +49,15 @@ typedef struct {
     uint32_t destination_reseller_cost;
     uint32_t destination_customer_cost;
 
-    char furnished_charging_info[256];
-    char header_diversion[2048];
-    char header_pai[2048];
-    char header_u2u[256];
-
-    char group[256];
-
     uint8_t split;
 
     mos_data_t mos;
 
-    char source_concurrent_calls_quota[32];
-    char source_concurrent_calls_count[32];
-    char source_concurrent_calls_count_customer[32];
-    char destination_concurrent_calls_quota[32];
-    char destination_concurrent_calls_count[32];
-    char destination_concurrent_calls_count_customer[32];
-    char source_last_hih[32];
-
-    char acc_ref[256];
     unsigned int intermediate:1;
 } cdr_entry_t;
+
+#undef F
+#undef FA
 
 int cdr_process_records(med_entry_t *records, uint64_t count, uint64_t *cdr_count, struct medmysql_batches *,
         int do_intermediate);
